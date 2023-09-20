61d01c7cb8bed557e3dacbad08921d75e8f9ef57
DevelopersathishG <145517772+DevelopersathishG@users.noreply.github.com>
2023-09-20 07:39:14 +0530

    Update Userrepository.java

--- ecom/ecom/src/main/java/com/ecommerce/repository/Userrepository.java
+++ ecom/ecom/src/main/java/com/ecommerce/repository/Userrepository.java
@@ -14,10 +14,10 @@ import com.ecommerce.entity.Userproduct;
 @Repository
 public interface Userrepository extends JpaRepository<Products, Long> {
 	
-//    @Query("SELECT p.product_name AS name, p.product_description AS description, p.category AS category, p.selling_price AS price FROM com.ecommerce.entity.Products p WHERE p.product_id = :productId")
-//    Userproduct findNameAndPriceById(Long productId);
-//
-// @Query("SELECT p.product_name AS name, p.product_description AS description, p.category AS category, p.selling_price AS price FROM com.ecommerce.entity.Products p")
+    @Query("SELECT p.product_name AS name, p.product_description AS description, p.category AS category, p.selling_price AS price FROM com.ecommerce.entity.Products p WHERE p.product_id = :productId")
+    Userproduct findNameAndPriceById(Long productId);
+
+ @Query("SELECT p.product_name AS name, p.product_description AS description, p.category AS category, p.selling_price AS price FROM com.ecommerce.entity.Products p")
     List<Userproduct> findAllLimitedDetails();
 	
 }
